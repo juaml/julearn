2bd9dc1a5222d02c31207484831af4ed4e8c77af
Sami Hamdan <sami.hamdan@fz-juelich.de>
2020-11-02 14:24:06 +0100

    MODIFY rest according to #17

--- julearn/model_selection.py
+++ julearn/model_selection.py
@@ -8,13 +8,8 @@ def wrap_search(searcher, *args, **kwargs):
         def __init__(self):
             super().__init__(*args, **kwargs)
 
-        def transform_confounds(self, X):
-            check_is_fitted(self)
-            return self.best_estimator_.transform_confounds(X)
-
         def transform_target(self, X, y):
             check_is_fitted(self)
-            X_conf_trans = self.transform_confounds(X)
-            return self.best_estimator_.transform_target(X_conf_trans, y)
+            return self.best_estimator_.transform_target(X, y)
 
     return wrap_searcher()
