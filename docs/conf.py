bd6fae0c0c4b55838aca5571544d5c3446c10a0f
samihamdan <sami.hamdan@hhu.de>
2020-11-19 14:16:14 +0100

    import not needed

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,6 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 from sphinx_gallery.sorting import ExplicitOrder
-import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
