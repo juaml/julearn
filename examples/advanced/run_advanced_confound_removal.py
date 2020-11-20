cc7a8e14b770a50a6922c4df8aea988dad9b66ba
samihamdan <sami.hamdan@hhu.de>
2020-11-20 13:58:27 +0100

    changes on example

--- examples/advanced/run_advanced_confound_removal.py
+++ examples/advanced/run_advanced_confound_removal.py
@@ -1,12 +1,13 @@
 """
-Confound Removal in Advanced Settings
-=====================================
+Return Confounds in Confound Removal
+====================================
 
 In most cases confound removal is a simple operation.
 You regress out the confound from the features and only continue working with
-these new deconfounded features. This example will not focus on these
-typical cases. Instead, this will show how to keep the confounds and use
-them as additional features.
+these new confound removed features. This is also the default setting for
+julearn's `remove_confound`_ step. But sometimes you want to work with the
+confound even after removing it from the features. In this example, we
+will discuss the options you have.
 
 .. include:: ../../links.inc
 """
@@ -15,10 +16,8 @@ them as additional features.
 # License: AGPL
 from sklearn.datasets import load_diabetes  # to load data
 from julearn.pipeline import create_extended_pipeline
-from julearn.transformers import (get_transformer,
-                                  DataFrameConfoundRemover,
-                                  ChangeColumnTypes)
-from julearn.estimators import get_model
+from julearn.transformers import get_transformer, ChangeColumnTypes
+from julearn import run_cross_validation
 
 
 # load in the data
@@ -39,6 +38,11 @@ print('Features: ', df_features.head())
 print('Target: ', target.describe())
 
 ###############################################################################
+# Now, we can put both into one DataFrame:
+data = df_features.copy()
+data['target'] = target
+
+###############################################################################
 # In the following we will explore different settings of confound removal
 # using Julearns pipeline functionalities.
 #
@@ -48,71 +52,55 @@ print('Target: ', target.describe())
 #
 # Confound Removal Typical Use Case
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# Here, we first deconfound the features.
+# Here, we first confound removed the features.
 # The confound is automatically dropped by the `.DataFrameConfoundRemover`.
 # Afterwards, we will transform our features with a pca and run
-# a lienar regression
+# a linear regression
 #
-typical_pipe = create_extended_pipeline(
-    preprocess_steps_features=[
-        ('remove_confound', DataFrameConfoundRemover(), 'subset', 'all'),
-        ('pca', *get_transformer('pca'))
-    ],
-    preprocess_steps_confounds=None,
-    preprocess_transformer_target=None,
-    model=('lr', get_model('linreg', 'regression')),
-    confounds=['sex'],
-    categorical_features=None,
-)
-typical_pipe.fit(df_features, target)
+feature_names = list(df_features.drop(columns='sex').columns)
+scores, model = run_cross_validation(
+    X=feature_names, y='target', data=data,
+    confounds='sex', model='linreg', problem_type='regression',
+    preprocess_X=['remove_confound', 'pca'],
+    return_estimator='final')
 
 ###############################################################################
 # We can use the `preprocess` method of the `.ExtendedDataFramePipeline`
-# to inspect the transformations/preprocessing steps of our pipeline.
+# to inspect the transformations/preprocessing steps of the returned estimator.
 # By providing a step name to the `until` argument of the
-# `preprocess` method we return the transformed X and y including to this step.
-# This output always includes the transformed
-# X and transformed y as a tuple.
-X_deconfounded, _ = typical_pipe.preprocess(
+# `preprocess` method we return the transformed X and y up to (inclusive)
+# the provided step.
+# This output consists of a tuple containing the transformed X and y,
+X_deconfounded, _ = model.preprocess(
     df_features, target, until='remove_confound')
 print(X_deconfounded.head())
 
-###############################################################################
-# As you can see the confound `sex` was dropped and only the deconfounded
-# features are used in the following pca.
+# As you can see the confound `sex` was dropped
+# and only the confound removed features are used in the following pca.
 # But what if you want to keep the confound in the feature space.
 #
-# For example, let's assume that you want to do a pca on the deconfounded
-# features, but still want to keep the confound as a feature.
-# This would mean that the following pca would also use the confound
-# as a feature.
+# For example, let's assume that you want to do a pca on the confound removed
+# feature, but want to keep the confound for the actual modelling step.
 # Let us have a closer look to the confound remover in order to understand
 # how we could achieve such a task:
 #
 # .. autoclass:: julearn.transformers.DataFrameConfoundRemover
 
 ###############################################################################
-# As you can see above we can set the `keep_confounds` argument to True,
-# if we want to keep the confounds.
+# Above you can see that we can set the `keep_confounds` argument to True.
+# This will keep the confounds after confound removal
 # Here is an example of how this can look like:
 
-keep_confound_pipe = create_extended_pipeline(
-    preprocess_steps_features=[
-        ('remove_confound', DataFrameConfoundRemover(
-            keep_confounds=True), 'subset', 'all'),
-        ('pca', *get_transformer('pca'))
-    ],
-    preprocess_steps_confounds=None,
-    preprocess_transformer_target=None,
-    model=('lr', get_model('linreg', 'regression')),
-    confounds=['sex'],
-    categorical_features=None,
-)
-keep_confound_pipe.fit(df_features, target)
+scores, model = run_cross_validation(
+    X=feature_names, y='target', data=data,
+    confounds='sex', model='linreg', problem_type='regression',
+    preprocess_X=['remove_confound', 'pca'],
+    model_params=dict(remove_confound__keep_confounds=True),
+    return_estimator='final')
 
 ###############################################################################
 # As you can see this will keep the confound
-X_deconfounded, _ = keep_confound_pipe.preprocess(
+X_deconfounded, _ = model.preprocess(
     df_features, target, until='remove_confound')
 print(X_deconfounded.head())
 
@@ -121,7 +109,7 @@ print(X_deconfounded.head())
 # This is the case because by default transformers only transform continuous
 # features (including features without a specified type)
 # and the confound is of type confound.
-X_transformed, _ = keep_confound_pipe.preprocess(df_features, target)
+X_transformed, _ = model.preprocess(df_features, target)
 print(X_transformed.head())
 
 # This means that the resulting Linear Regression will use the deconfounded
@@ -134,26 +122,22 @@ print(X_transformed.head())
 # which return the same columns or you can use the
 # `.ChangeColumnTypes` to change the returned confounds
 # to a continuous variable like this:
-confound_as_feature_pipe = create_extended_pipeline(
-    preprocess_steps_features=[
-        ('remove_confound', DataFrameConfoundRemover(
-            keep_confounds=True), 'subset', 'all'),
-        ('change_column_types', ChangeColumnTypes('.*confound', 'continuous'),
-         'from_transformer', 'all'),
-        ('pca', *get_transformer('pca'))
-    ],
-    preprocess_steps_confounds=None,
-    preprocess_transformer_target=None,
-    model=('lr', get_model('linreg', 'regression')),
-    confounds=['sex'],
-    categorical_features=None,
-)
-confound_as_feature_pipe.fit(df_features, target)
+scores, model = run_cross_validation(
+    X=feature_names, y='target', data=data,
+    confounds='sex', model='linreg', problem_type='regression',
+    preprocess_X=['remove_confound',
+                  ('change_column_types',
+                   ChangeColumnTypes('.*confound', 'continuous'),
+                   'from_transformer', 'all'),
+                  'pca'],
+    model_params=dict(remove_confound__keep_confounds=True),
+    return_estimator='final')
+
 
 ###############################################################################
 # As you can see this will keep the confound and
 # change its type to a continuous variable.
-X_deconfounded, _ = confound_as_feature_pipe.preprocess(
+X_deconfounded, _ = model.preprocess(
     df_features, target, until='change_column_types',
     return_trans_column_type=True)
 print(X_deconfounded.head())
@@ -161,5 +145,5 @@ print(X_deconfounded.head())
 ###############################################################################
 # Because the confound is treated as a normal continuous feature
 # after removal it will be transformed in the pca as well
-X_transformed, _ = confound_as_feature_pipe.preprocess(df_features, target)
+X_transformed, _ = model.preprocess(df_features, target)
 print(X_transformed.head())
