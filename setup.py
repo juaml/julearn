9f223bc5af0d7022ed365053f8d9343911ed5ff5
Federico Raimondo <federaimondo@gmail.com>
2020-12-09 17:09:48 +0100

    Fix for #91

--- setup.py
+++ setup.py
@@ -1,17 +1,7 @@
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
 #          Sami Hamdan <s.hamdan@fz-juelich.de>
 # License: AGPL
-from pathlib import Path
 import setuptools
-
-version = None
-with open(Path('julearn') / '_version.py', 'r') as fid:
-    for line in (line.strip() for line in fid):
-        if line.startswith('__version__'):
-            version = line.split('=')[1].strip().strip('\'')
-            break
-if version is None:
-    raise RuntimeError('Could not determine version')
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
@@ -22,7 +12,6 @@ URL = 'https://juaml.github.io/julearn'
 
 setuptools.setup(
     name='julearn',
-    version=version,
     author='Applied Machine Learning',
     author_email='sami.hamdan@fz-juelich.de',
     description='FZJ AML Library ',
@@ -52,4 +41,11 @@ setuptools.setup(
                       'pandas>=1.1.2',
                       'scikit-learn>=0.23.2'],
     python_requires='>=3.6',
+    use_scm_version=dict(
+        version_scheme="python-simplified-semver",
+        local_scheme="node-and-date",
+        write_to="julearn/_version.py",
+        write_to_template="__version__ = '{version}'"
+    ),
+    setup_requires=['setuptools_scm'],
 )
