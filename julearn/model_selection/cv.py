5c476acb882cc8b43a8719469dc2564dc148a774
Fede Raimondo <federaimondo@gmail.com>
2021-05-06 15:11:51 +0200

    (Repeated)StratifiedGroupKFold

--- julearn/model_selection/cv.py
+++ julearn/model_selection/cv.py
@@ -4,7 +4,10 @@
 import numpy as np
 
 from sklearn.model_selection._split import (BaseShuffleSplit,
+                                            StratifiedKFold,
+                                            _RepeatedSplits,
                                             _validate_shuffle_split)
+from sklearn.utils.validation import check_array
 
 
 class StratifiedBootstrap(BaseShuffleSplit):
@@ -92,3 +95,82 @@ class StratifiedBootstrap(BaseShuffleSplit):
         to an integer.
         """
         return super().split(X, y, groups)
+
+
+class StratifiedGroupsKFold(StratifiedKFold):
+    def split(self, X, y, groups=None):
+        """Generate indices to split data into training and test set.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data, where n_samples is the number of samples
+            and n_features is the number of features.
+            Note that providing ``y`` is sufficient to generate the splits and
+            hence ``np.zeros(n_samples)`` may be used as a placeholder for
+            ``X`` instead of actual training data.
+        y : array-like of shape (n_samples,)
+            Always ignored, exists for compatibility.
+        groups : object
+            The stratification variable.
+        Yields
+        ------
+        train : ndarray
+            The training set indices for that split.
+        test : ndarray
+            The testing set indices for that split.
+        Notes
+        -----
+        Randomized CV splitters may return different results for each call of
+        split. You can make the results identical by setting `random_state`
+        to an integer.
+        """
+        groups = check_array(groups, ensure_2d=False, dtype=None)
+        return super().split(X, groups, None)
+
+
+class RepeatedStratifiedGroupsKFold(_RepeatedSplits):
+    """Repeated Stratified K-Fold cross validator.
+    Repeats Stratified K-Fold n times with different randomization in each
+    repetition.
+    Read more in the :ref:`User Guide <repeated_k_fold>`.
+    Parameters
+    ----------
+    n_splits : int, default=5
+        Number of folds. Must be at least 2.
+    n_repeats : int, default=10
+        Number of times cross-validator needs to be repeated.
+    random_state : int, RandomState instance or None, default=None
+        Controls the generation of the random states for each repetition.
+        Pass an int for reproducible output across multiple function calls.
+        See :term:`Glossary <random_state>`.
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from sklearn.model_selection import RepeatedStratifiedKFold
+    >>> X = np.array([[1, 2], [3, 4], [1, 2], [3, 4]])
+    >>> y = np.array([0, 0, 1, 1])
+    >>> rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=2,
+    ...     random_state=36851234)
+    >>> for train_index, test_index in rskf.split(X, y):
+    ...     print("TRAIN:", train_index, "TEST:", test_index)
+    ...     X_train, X_test = X[train_index], X[test_index]
+    ...     y_train, y_test = y[train_index], y[test_index]
+    ...
+    TRAIN: [1 2] TEST: [0 3]
+    TRAIN: [0 3] TEST: [1 2]
+    TRAIN: [1 3] TEST: [0 2]
+    TRAIN: [0 2] TEST: [1 3]
+    Notes
+    -----
+    Randomized CV splitters may return different results for each call of
+    split. You can make the results identical by setting `random_state`
+    to an integer.
+    See Also
+    --------
+    RepeatedKFold : Repeats K-Fold n times.
+    """
+    def __init__(self, *, n_splits=5, n_repeats=10, random_state=None):
+        super().__init__(
+            StratifiedGroupsKFold, n_repeats=n_repeats,
+            random_state=random_state,
+            n_splits=n_splits)
