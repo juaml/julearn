c84d093d2afc42ed45ec97a0438a99b49e38eab8
samihamdan <sami.hamdan@hhu.de>
2021-05-06 16:42:45 +0200

    Add testing pred and pred_proba

--- julearn/estimators/tests/test_dynamic.py
+++ julearn/estimators/tests/test_dynamic.py
@@ -40,26 +40,35 @@ def test_algorithms():
 
         # julearn
         np.random.seed(seed)
-        ensemble_model = RandomForestClassifier()
+        ensemble_model = RandomForestClassifier(random_state=6)
         dynamic_model = DynamicSelection(
             ensemble=ensemble_model, algorithm=algorithm,
             random_state=seed, random_state_algorithm=seed, ds_split=ds_split)
         dynamic_model.fit(df_iris[X], df_iris[y])
         score_julearn = dynamic_model.score(df_iris[X], df_iris[y])
+        pred_julearn = dynamic_model.predict(df_iris[X])
 
         # deslib
         np.random.seed(seed)
         X_train, X_dsel, y_train, y_dsel = train_test_split(
             df_iris[X], df_iris[y], test_size=ds_split, random_state=seed)
 
-        pool_classifiers = RandomForestClassifier()
+        pool_classifiers = RandomForestClassifier(random_state=6)
         pool_classifiers.fit(X_train, y_train)
 
         cls = all_algorithms[algorithm]
         model_deslib = cls(pool_classifiers, random_state=seed)
         model_deslib.fit(X_dsel, y_dsel)
         score_deslib = model_deslib.score(df_iris[X], df_iris[y])
+        pred_deslib = model_deslib.predict(df_iris[X])
+
         assert score_deslib == score_julearn
+        assert (pred_deslib == pred_julearn).all()
+
+        if hasattr(model_deslib, 'predict_proba'):
+            pred_proba_julearn = dynamic_model.predict_proba(df_iris[X])
+            pred_proba_deslib = model_deslib.predict_proba(df_iris[X])
+            assert (pred_proba_deslib == pred_proba_julearn).all()
 
 
 def test_wrong_algo():
