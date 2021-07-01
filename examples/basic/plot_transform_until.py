0c8f2164e4422c7d2f81d0e8af4aa6c76e7f7a51
More, Shammi <s.more@fz-juelich.de>
2021-07-01 15:56:13 +0200

    updated transform_until

--- examples/basic/plot_transform_until.py
+++ examples/basic/plot_transform_until.py
@@ -3,8 +3,8 @@ Preprocessing with variance threshold, zscore and PCA
 =======================================================
 
 This example uses the 'iris' dataset, performs simple binary
-classification after the pre-processing the features including removal of low 
-variance features, feature normalization using zscore and feature reduction 
+classification after the pre-processing the features including removal of low
+variance features, feature normalization using zscore and feature reduction
 using PCA. We will check the features after each preprocessing step.
 
 """
@@ -46,15 +46,15 @@ y = 'species'
 print('Summary Statistics of the raw features : \n', df_iris.describe())
 
 ###############################################################################
-# We will preprocess the features using variance thresholding, zscore and PCA 
+# We will preprocess the features using variance thresholding, zscore and PCA
 # and then train a random forest model
 
 # Define the model parameters and preprocessing steps first
 # Setting the threshold for variance to 0.15, number of PCA components to 2
 # and number of trees for random forest to 200
 
-model_params = {'select_variance__threshold': 0.15, 
-                'pca__n_components': 2, 
+model_params = {'select_variance__threshold': 0.15,
+                'pca__n_components': 2,
                 'rf__n_estimators': 200}
 
 preprocess_X = ['select_variance', 'zscore', 'pca']
@@ -66,14 +66,13 @@ scores, model = run_cross_validation(
 
 ###############################################################################
 # Now let's look at the data after pre-processing. It can be done using
-# 'preprocess' method. By default it will apply all the pre-processing steps 
-# ('select_variance', 'zscore', 'pca' in this case) and return pre-processed 
-# data. Note that here we are applying pre-processing only on X.
- 
+# 'preprocess' method. By default it will apply all the pre-processing steps
+# (`'select_variance'`, `'zscore'`, `'pca'` in this case) and return
+# pre-processed data. Note that here we are applying pre-processing only on X.
+# Notice that the column names have changed in this new dataframe.
+
 pre_X, pre_y = model.preprocess(df_iris[X], df_iris[y])
 print('Features after PCA : \n', pre_X)
-
-# Notice that the column names have changed in this new dataframe.
 
 # Let's plot scatter plots for raw features and the PCA components
 pre_df = pre_X.join(pre_y)
@@ -87,21 +86,21 @@ axes[1].set_title('PCA components')
 
 ###############################################################################
 # But let's say we want to look at features after applying only one or more
-# preprocessing steps eg: only variance thresholding or till zscore. 
+# preprocessing steps eg: only variance thresholding or till zscore.
 # To do so we can set the argument `until` to the desired preprocessing step.
-# Note that the name of the preprocessing step is the same as used in the 
-# `run_cross_validation` function in `preprocess_X`. 
+# Note that the name of the preprocessing step is the same as used in the
+# `run_cross_validation` function in `preprocess_X`.
 
-# Let's look at features after variance thresholding. We see that now we have 
-# one feature less as the variance for this feature ('sepal_width') was below 
+# Let's look at features after variance thresholding. We see that now we have
+# one feature less as the variance for this feature ('sepal_width') was below
 # the set threshold.
 
-var_th_X, var_th_y = model.preprocess(df_iris[X], df_iris[y], 
-until='select_variance')
+var_th_X, var_th_y = model.preprocess(df_iris[X], df_iris[y],
+                                      until='select_variance')
 print('Features after variance thresholding: \n', var_th_X)
 
 ###############################################################################
-# Now let's see features after variance thresholding and zscoring. We can now 
+# Now let's see features after variance thresholding and zscoring. We can now
 # set the `until` argument to `'zscore'`
 
 zscored_X, zscored_y = model.preprocess(df_iris[X], df_iris[y], until='zscore')
@@ -114,7 +113,7 @@ print('Summary Statistics of the zscored features : \n', zscored_df.describe())
 
 ###############################################################################
 # We can also look at the features pre-processed till PCA. Since `'pca'` is the
-# last preprocessing step we don't really need the `until` argument 
+# last preprocessing step we don't really need the `until` argument
 # (as shown above).
 
 pre_X, pre_y = model.preprocess(df_iris[X], df_iris[y], until='pca')
