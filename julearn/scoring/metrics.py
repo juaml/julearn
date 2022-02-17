592d804a944bbcd5ab8f81f9c15032c72447b023
Fede Raimondo <federaimondo@gmail.com>
2022-02-17 16:13:10 +0100

    not my day

--- julearn/scoring/metrics.py
+++ julearn/scoring/metrics.py
@@ -1,12 +1,14 @@
 import numpy as np
 
+
 def ensure_1d(y):
     if isinstance(y, np.ndarray):
         if y.ndim > 1:
             y = np.squeeze(y)
         if y.ndim > 1:
             raise ValueError('y must be 1d')
-    return y    
+    return y
+
 
 def r2_corr(y_true, y_pred):
     return np.corrcoef(ensure_1d(y_true), ensure_1d(y_pred))[0, 1]**2
