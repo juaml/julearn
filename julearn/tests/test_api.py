c1a84517c70b31b0b1448f9338e727bcc2f356b3
Sami Hamdan <sami.hamdan@hhu.de>
2022-06-16 21:17:54 +0200

    Reverting only to 100% support sklearn 1.0

--- julearn/tests/test_api.py
+++ julearn/tests/test_api.py
@@ -304,7 +304,7 @@ def test_consistency():
     # Warn with target transformer
     with pytest.warns(RuntimeWarning, match='not suitable for'):
         _ = run_cross_validation(X=X, y=y, data=df_iris, model='svm', cv=cv,
-                                 preprocess_y=LabelBinarizer())
+                                 preprocess_y='zscore')
 
     # Error for regression
     with pytest.raises(ValueError, match='not suitable for'):
