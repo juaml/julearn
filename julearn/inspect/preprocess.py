d2a839e345b69c1e3e8e6cd652570b4b66495e34
Sami Hamdan <sami.hamdan@hhu.de>
2022-12-02 15:38:03 +0100

    Modify inspect

--- julearn/inspect/preprocess.py
+++ julearn/inspect/preprocess.py
@@ -1,14 +1,15 @@
 from julearn.utils import raise_error
 
 
-def preprocess(pipeline, X,  until=None):
+def preprocess(pipeline, X, df, until=None):
 
+    _X = df[X]
     if until is None:
-        return pipeline[:-1].transform(X)
+        return pipeline[:-1].transform(df[_X])
 
     for i, (name, _) in enumerate(pipeline.steps[:-1]):
         if name.replace("wrapped_", "") == until.replace("wrapped_", ""):
             break
     else:
         raise_error(f"No {until} found.")
-    return pipeline[:i+1].transform(X)
+    return pipeline[:i+1].transform(_X)
