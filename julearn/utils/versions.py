0fc853b4b065255e5f3ae564bb83afd6ab8aaea2
Fede <fraimondo@proton.me>
2022-12-05 20:38:31 +0100

    Trigger CI

--- julearn/utils/versions.py
+++ julearn/utils/versions.py
@@ -1,8 +1,9 @@
 import sklearn
 
 
-def check_version(version,
-                  major_check=None, minor_check=None, patch_check=None):
+def check_version(
+    version, major_check=None, minor_check=None, patch_check=None
+):
     """check a version following major.minor.patch version numbers
     according to checks as functions major, minor and patch.
 
@@ -31,9 +32,9 @@ def check_version(version,
         return lambda x: True if check_func is None else check_func(x)
 
     version_checks = [major_check, minor_check, patch_check]
-    versions_checked = [get_check(check)(version)
-                        for version, check in zip(
-        version.split('.'), version_checks)
+    versions_checked = [
+        get_check(check)(version)
+        for version, check in zip(version.split("."), version_checks)
     ]
 
     return all(versions_checked)
@@ -41,11 +42,12 @@ def check_version(version,
 
 def _joblib_parallel_args(**kwargs):
     sklearn_version = sklearn.__version__
-    higher_than_11 = check_version(sklearn_version,
-                                   lambda x: int(x) >= 1,
-                                   lambda x: int(x) >= 1)
+    higher_than_11 = check_version(
+        sklearn_version, lambda x: int(x) >= 1, lambda x: int(x) >= 1
+    )
     if higher_than_11:
         return kwargs
     else:
         from sklearn.utils.fixes import _joblib_parallel_args as _sk_parallel
+
         return _sk_parallel(**kwargs)
