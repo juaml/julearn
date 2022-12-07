1dcf57eb5629b9f50d04c6980f9672bb7a85913b
Vera Komeyer <v.komeyer@fz-juelich.de>
2022-12-07 14:30:46 +0100

    Adapt HPT example to new api - to be checked after bug fix HPT

--- examples/advanced/dis_run_hyperparameter_tuning.py
+++ examples/advanced/dis_run_hyperparameter_tuning.py
@@ -1,9 +1,9 @@
 """
 Tuning Hyperparameters
-=======================
+======================
 
 This example uses the 'fmri' dataset, performs simple binary classification
-using a Support Vector Machine classifier and analyse the model.
+using a Support Vector Machine classifier and analyses the model.
 
 
 References
@@ -20,6 +20,7 @@ cognitive control in context-dependent decision-making. Cerebral Cortex.
 import numpy as np
 from seaborn import load_dataset
 
+from julearn.pipeline import PipelineCreator
 from julearn import run_cross_validation
 from julearn.utils import configure_logging
 
@@ -47,110 +48,133 @@ df_fmri = df_fmri.reset_index()
 print(df_fmri.head())
 
 ###############################################################################
-# Lets do a first attempt and use a linear SVM with the default parameters.
-model_params = {'svm__kernel': 'linear'}
+# Let's do a first attempt and use a linear SVM with the default parameters.
 X = ['frontal', 'parietal']
 y = 'event'
-scores = run_cross_validation(
-    X=X, y=y, data=df_fmri, model='svm', preprocess='zscore',
-    model_params=model_params)
 
-print(scores['test_score'].mean())
+# Create a pipeline with z-score preprocessing and SVM
+creator = (PipelineCreator()
+            .add("zscore", apply_to="*")  # all columns considered continous
+            .add("svm", problem_type="classification", kernel="linear"))
+
+scores = run_cross_validation(
+    X=X, y=y, data=df_fmri, model=creator)
+
+print(f"Test score: {scores['test_score'].mean()}")
 
 ###############################################################################
 # The score is not so good. Lets try to see if there is an optimal
 # regularization parameter (C) for the linear SVM.
-model_params = {
-    "svm__kernel": "linear",
-    "svm__C": [0.01, 0.1],
-    "cv": 2,
-}  # CV=2 too speed up the example
 X = ["frontal", "parietal"]
 y = "event"
+
+# Create a pipeline with z-score preprocessing and SVM
+creator = (PipelineCreator()
+            .add("zscore", apply_to="*")  # all columns considered continous
+            .add("svm", problem_type="classification",
+                 kernel="linear", C=[0.01, 0.1]))  # TODO HPT bug -> wait for Sami to be fixed
+model_params = {
+    "cv": 2,  # speed up the example
+}
+
 scores, estimator = run_cross_validation(
     X=X,
     y=y,
     data=df_fmri,
-    model="svm",
-    preprocess="zscore",
+    model=creator,
     model_params=model_params,
     return_estimator="final",
 )
 
-print(scores["test_score"].mean())
+print(f"Test score: {scores['test_score'].mean()}")
 
 ###############################################################################
 # This did not change much, lets explore other kernels too.
-model_params = {
-    "svm__kernel": ["linear", "rbf", "poly"],
-    "svm__C": [0.01, 0.1],
-    "cv": 2,
-}  # CV=2 too speed up the example
 X = ["frontal", "parietal"]
 y = "event"
+
+# Create a pipeline with z-score preprocessing and SVM
+creator = (PipelineCreator()
+            .add("zscore", apply_to="*")  # all columns considered continous
+            .add("svm", problem_type="classification",
+                 kernel=["linear", "rbf", "poly"],
+                 C=[0.01, 0.1]))  # TODO HPT bug -> wait for Sami to be fixed
+model_params = {
+    "cv": 2,  # speed up the example
+}
+
 scores, estimator = run_cross_validation(
     X=X,
     y=y,
     data=df_fmri,
-    model="svm",
-    preprocess="zscore",
+    model=creator,
     model_params=model_params,
     return_estimator="final",
 )
 
-print(scores["test_score"].mean())
+print(f"Test score: {scores['test_score'].mean()}")
 ###############################################################################
 # It seems that we might have found a better model, but which one is it?
-print(estimator.best_params_)
+print(f"Best parameters of final estimator {estimator.best_params_}")  # TODO double check after HPT works
 
 ###############################################################################
 # Now that we know that a RBF kernel is better, lest test different *gamma*
 # parameters.
-model_params = {
-    "svm__kernel": "rbf",
-    "svm__C": [0.01, 0.1],
-    "svm__gamma": [1e-2, 1e-3],
-    "cv": 2,
-}  # CV=2 too speed up the example
 X = ["frontal", "parietal"]
 y = "event"
+
+# Create a pipeline with z-score preprocessing and SVM
+creator = (PipelineCreator()
+            .add("zscore", apply_to="*")  # all columns considered continous
+            .add("svm", problem_type="classification",
+                 kernel=["rbf"],
+                 C=[0.01, 0.1],
+                 gamma=[1e-2, 1e-3]))  # TODO HPT bug -> wait for Sami to be fixed
+model_params = {
+    "cv": 2,  # speed up the example
+}
+
 scores, estimator = run_cross_validation(
     X=X,
     y=y,
     data=df_fmri,
-    model="svm",
-    preprocess="zscore",
+    model=creator,
     model_params=model_params,
     return_estimator="final",
 )
 
-print(scores["test_score"].mean())
-print(estimator.best_params_)
+print(f"Test score: {scores['test_score'].mean()}")
+print(f"Best parameters of final estimator {estimator.best_params_}")  # TODO double check after HPT works
 
 ###############################################################################
 # It seems that without tuning the gamma parameter we had a better accuracy.
 # Let's add the default value and see what happens.
-model_params = {
-    "svm__kernel": "rbf",
-    "svm__C": [0.01, 0.1],
-    "svm__gamma": [1e-2, 1e-3, "scale"],
-    "cv": 2,
-}  # CV=2 too speed up the example
 X = ["frontal", "parietal"]
 y = "event"
+
+# Create a pipeline with z-score preprocessing and SVM
+creator = (PipelineCreator()
+            .add("zscore", apply_to="*")  # all columns considered continous
+            .add("svm", problem_type="classification",
+                 kernel=["rbf"],
+                 C=[0.01, 0.1],
+                 gamma=[1e-2, 1e-3, "scale"]))  # TODO HPT bug -> wait for Sami to be fixed
+model_params = {
+    "cv": 2,  # speed up the example
+}
+
 scores, estimator = run_cross_validation(
     X=X,
     y=y,
     data=df_fmri,
-    model="svm",
-    preprocess="zscore",
+    model=creator,
     model_params=model_params,
     return_estimator="final",
 )
 
-print(scores["test_score"].mean())
-print(estimator.best_params_)
+print(f"Test score: {scores['test_score'].mean()}")
+print(f"Best parameters of final estimator {estimator.best_params_}")  # TODO double check after HPT works
 
 ###############################################################################
 # So what was the best ``gamma`` in the end?
-print(estimator.best_estimator_["svm"]._gamma)
+print(estimator.best_estimator_["svm"]._gamma)   # TODO double check after HPT works
