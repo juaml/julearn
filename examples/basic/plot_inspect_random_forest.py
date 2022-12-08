d908df7a1428f7f252c1319d3aa7e09a5a881603
Nieto Nicolas <nnieto@sinc.unl.edu.ar>
2022-12-08 08:35:40 -0300

    modified run simple classification and inspect RF

--- examples/basic/plot_inspect_random_forest.py
+++ examples/basic/plot_inspect_random_forest.py
@@ -1,14 +1,21 @@
-"""
+""".
+
 Inspecting Random Forest models
 ===============================
 
 This example uses the 'iris' dataset, performs simple binary classification
-using a Random Forest classifier and analyse the model.
+using a Random Forest classifier. We will also analyse the features used
+by the model to perform the classification.
+
+References
+----------
+Fisher, R. A. (1936). The use of multiple measurements in taxonomic problems.
+Annals of eugenics, 7(2), 179-188.
 
 .. include:: ../../links.inc
 """
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
-#
+#          Nieto Nicolás <n.nieto@fz-juelich.de>
 # License: AGPL
 import pandas as pd
 
@@ -18,101 +25,123 @@ from seaborn import load_dataset
 
 from julearn import run_cross_validation
 from julearn.utils import configure_logging
+from sklearn.model_selection import StratifiedKFold
 
 ###############################################################################
 # Set the logging level to info to see extra information
-configure_logging(level="INFO")
+configure_logging(level='INFO')
 
 ###############################################################################
-# Random Forest variable importance
+# Iris classification with Random Forest - variable importance analysis.
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
 
-df_iris = load_dataset("iris")
+###############################################################################
+# Load iris dataset from `seaborn` library. It contains information about
+# three flowers: `species`, `versicolor`,`virginica` and `setosa`.
+# For each observation, it has four features: `sepal_length`, `sepal_width`,
+# `petal_length` and `petal_width`.
+#
+# The data needs to be a `pandas.DataFrame`
+df_iris = load_dataset('iris')
 
 ###############################################################################
-# The dataset has three kind of species. We will keep two to perform a binary
-# classification.
-df_iris = df_iris[df_iris["species"].isin(["versicolor", "virginica"])]
-
-X = ["sepal_length", "sepal_width", "petal_length"]
-y = "species"
-
+# The dataset has three kind of species. We will keep only two of them
+# to perform a binary classification.
+df_iris = df_iris[df_iris['species'].isin(['versicolor', 'virginica'])]
 
 ###############################################################################
-# We will use a Random Forest classifier. By setting
-# `return_estimator='final'`, the :func:`.run_cross_validation` function
-# returns the estimator fitted with all the data.
+# As features, we will only use sepal length, width and petal length.
+# We set a list with the name of the columns we want to use as features.
+# Those are columns names of `df_iris`
+X = ['sepal_length', 'sepal_width', 'petal_length']
+
+###############################################################################
+
+# As target, we will use the species, as we want to predict the specie with the
+# information about the petal and sepal.
+# We set a variable with the name of the column we want to use as a target.
+# The target is also included in `df_iris`
+y = 'species'
+
+###############################################################################
+# If we take a look at the data, we will find that the samples are sorted by
+# the target. In that case, a problem will occur if we use a simple Kfold
+# approach, as some splits will contain only samples from one class.
+#
+# To solve this, we will use a Stratified KFold approach, that takes care of
+# this, and makes sure that every split have the same proportion of data for
+# each class.
+cv = StratifiedKFold(n_splits=5)
+
+###############################################################################
+# We will use a Random Forest classifier. We will set
+# `return_estimator='final'`, so the :func:`.run_cross_validation` function
+# will return the model fitted with all the data. We will then analyse the
+# most relevant features used by the model.
 
 scores, model_iris = run_cross_validation(
-    X=X,
-    y=y,
-    data=df_iris,
-    model="rf",
-    preprocess="zscore",
-    problem_type="classification",
-    return_estimator="final",
-)
+    X=X, y=y, data=df_iris, model='rf', preprocess='zscore', cv=cv,
+    return_estimator='final')
 
 ###############################################################################
 # This type of classifier has an internal variable that can inform us on how
-# _important_ is each of the features. Caution: read the proper scikit-learn
-# documentation (`Random Forest`_)
-rf = model_iris["rf"]
+# _important_ is each of the features. Please, read the proper scikit-learn
+# documentation (`Random Forest`_) for more information.
+rf = model_iris['rf']
 
-to_plot = pd.DataFrame(
-    {
-        "variable": [x.replace("_", " ") for x in X],
-        "importance": rf.feature_importances_,
-    }
-)
+to_plot = pd.DataFrame({
+    'variable': [x.replace('_', ' ') for x in X],
+    'importance': rf.feature_importances_
+})
 
 fig, ax = plt.subplots(1, 1, figsize=(6, 4))
-sns.barplot(x="importance", y="variable", data=to_plot, ax=ax)
-ax.set_title("Variable Importances for Random Forest Classifier")
+sns.barplot(x='importance', y='variable', data=to_plot, ax=ax)
+ax.set_title('Variable Importances for Random Forest Classifier')
 fig.tight_layout()
 
 ###############################################################################
-# However, some reviewers (including myself), might wander about the
-# variability of the importance of these features. In the previous example
-# all the feature importances were obtained by fitting on the entire dataset,
-# while the performance was estimated using cross validation.
+# However, one might wander about the variability of the importance of these
+# features. In the previous example all the feature importances were obtained
+# by fitting on the entire dataset, while the performance was estimated using
+# cross validation. This set up is a better choice if what we want is a final
+# model that is trained with all the available data and will be used over an
+# unseen data.
 #
 # By specifying `return_estimator='cv'`, we can get, for each fold, the fitted
-# estimator.
+# estimator and analyse how important were each feature for each estimator.
+# If the feature importance large vary across folds, this could indicate a
+# typical case of overffiting, where the estimator heavily relies in one
+# feature.
+#
+# Note that now, all the models will be saved in a 'estimator' column.
 
 scores = run_cross_validation(
-    X=X,
-    y=y,
-    data=df_iris,
-    model="rf",
-    preprocess="zscore",
-    problem_type="classification",
-    return_estimator="cv",
-)
+    X=X, y=y, data=df_iris, model='rf',  preprocess='zscore', cv=cv,
+    return_estimator='cv')
 
 ###############################################################################
-# Now we can obtain the feature importance for each estimator (CV fold)
+# Now we can obtain the feature importance for each estimator, trained over
+# different CV folds and see the variance of the feature impotance across each
+# fold and repetition. We will obtain a total of 5 points, corresponding to
+# each fold.
+
 to_plot = []
-for i_fold, estimator in enumerate(scores["estimator"]):
-    this_importances = pd.DataFrame(
-        {
-            "variable": [x.replace("_", " ") for x in X],
-            "importance": estimator["rf"].feature_importances_,
-            "fold": i_fold,
-        }
-    )
+for i_fold, estimator in enumerate(scores['estimator']):
+    this_importances = pd.DataFrame({
+        'variable': [x.replace('_', ' ') for x in X],
+        'importance': estimator['rf'].feature_importances_,
+        'fold': i_fold
+    })
     to_plot.append(this_importances)
 
 to_plot = pd.concat(to_plot)
 
 ###############################################################################
-# Finally, we can plot the variable importances for each fold
+# Finally, we can plot the variable importances for each fold and repetition.
 
 fig, ax = plt.subplots(1, 1, figsize=(6, 4))
-sns.swarmplot(x="importance", y="variable", data=to_plot, ax=ax)
-ax.set_title(
-    "Distribution of variable Importances for Random Forest "
-    "Classifier across folds"
-)
+sns.swarmplot(x='importance', y='variable', data=to_plot, ax=ax)
+ax.set_title('Distribution of variable Importances for Random Forest '
+             'Classifier across folds')
 fig.tight_layout()
--- examples/basic/run_simple_binary_classification.py
+++ examples/basic/run_simple_binary_classification.py
@@ -1,47 +1,91 @@
-"""
+""".
+
 Simple Binary Classification
 ============================
 
 This example uses the 'iris' dataset and performs a simple binary
 classification using a Support Vector Machine classifier.
 
+We demonstrate how to use the main julearn function "run_cross_validation".
+We also show how to use some of the most basic features provided
+by this function.
+
+References
+----------
+Fisher, R. A. (1936). The use of multiple measurements in taxonomic problems.
+Annals of eugenics, 7(2), 179-188.
+
 .. include:: ../../links.inc
 """
 # Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
-#
+#          Nicolás Nieto <n.nieto@fz-juelich.de>
 # License: AGPL
 from seaborn import load_dataset
 from julearn import run_cross_validation
 from julearn.utils import configure_logging
-
+from sklearn.model_selection import StratifiedKFold
 ###############################################################################
 # Set the logging level to info to see extra information
-configure_logging(level="INFO")
+configure_logging(level='INFO')
 
 ###############################################################################
-df_iris = load_dataset("iris")
+# Load iris dataset from `seaborn` library. It contains information about
+# three flowers: `species`, `versicolor`,`virginica` and `setosa`.
+# For each observation, it has four features: `sepal_length`, `sepal_width`,
+# `petal_length` and `petal_width`.
+#
+# The data needs to be a `pandas.DataFrame`
+df_iris = load_dataset('iris')
 
 ###############################################################################
-# The dataset has three kind of species. We will keep two to perform a binary
-# classification.
-df_iris = df_iris[df_iris["species"].isin(["versicolor", "virginica"])]
+# The dataset has three kind of species. We will keep only two of them
+# to perform a binary classification.
+df_iris = df_iris[df_iris['species'].isin(['versicolor', 'virginica'])]
 
 ###############################################################################
-# As features, we will use the sepal length, width and petal length.
-# We will try to predict the species.
+# As features, we will only use sepal length, width and petal length.
+# We set a list with the name of the columns we want to use as features.
+# Those are columns names of `df_iris`
+X = ['sepal_length', 'sepal_width', 'petal_length']
 
-X = ["sepal_length", "sepal_width", "petal_length"]
-y = "species"
+###############################################################################
+
+# As target, we will use the species, as we want to predict the specie with the
+# information about the petal and sepal.
+# We set a variable with the name of the column we want to use as a target.
+# The target is also included in `df_iris`
+y = 'species'
+
+###############################################################################
+# We provide the model name as a string. We will use a Support Vector Machine.
+# For all available models see `models.available_models`_
+# Additionally, as a standard step in a classification pipeline, we will
+# z-score the data. We will provide the preprocess as a string.
+model = 'svm'
+preprocess = 'zscore'
+
+###############################################################################
+# If we take a look at the data, we will find that the samples are sorted by
+# the target. In that case, a problem will occur if we use a simple Kfold
+# approach, as some splits will contain only samples from one class.
+#
+# To solve this, we will use a Stratified KFold approach, that takes care of
+# this, and makes sure that every split have the same proportion of data for
+# each class.
+cv = StratifiedKFold(n_splits=5)
+
+###############################################################################
+# 'run_cross_validation'is the main function in julearn.
+# It will run a complete pipeline and return the test scores for all the folds.
+# The function returns different information about the execution.
+# We will print the mean test accuracy obtained in all folders (5 by default).
+#
+# Note that 'df_iris' contains all the information and X and y are only list of
+# strings or just a strings with the column names used.
 scores = run_cross_validation(
-    X=X,
-    y=y,
-    data=df_iris,
-    model="svm",
-    problem_type="classification",
-    preprocess="zscore",
-)
+    X=X, y=y, data=df_iris, model=model, preprocess=preprocess, cv=cv)
 
-print(scores["test_score"])
+print('Mean Test Accuracy: %f ' % scores['test_score'].mean())
 
 ###############################################################################
 # Additionally, we can choose to assess the performance of the model using
@@ -49,29 +93,25 @@ print(scores["test_score"])
 #
 # For example, we might have an unbalanced dataset:
 
-df_unbalanced = df_iris[20:]  # drop the first 20 versicolor samples
-print(df_unbalanced["species"].value_counts())
+df_unbalanced = df_iris[20:]  # drop the first 20 ve10rsicolor samples
+print('New number of samples for each class:')
+print(df_unbalanced['species'].value_counts())
+
 
 ###############################################################################
 # If we compute the `accuracy`, we might not account for this imbalance. A more
 # suitable metric is the `balanced_accuracy`. More information in scikit-learn:
-# `Balanced Accuracy`_
+# `Balanced Accuracy`.
 #
 # We will also set the random seed so we always split the data in the same way.
+# julearn is able to return several metrics. We need to pass the desired
+# metrics in a list.
 scores = run_cross_validation(
-    X=X,
-    y=y,
-    data=df_unbalanced,
-    model="svm",
-    seed=42,
-    preprocess="zscore",
-    problem_type="classification",
-    scoring=["accuracy", "balanced_accuracy"],
-)
+    X=X, y=y, data=df_unbalanced, model=model, seed=42, cv=cv,
+    preprocess=preprocess, scoring=['accuracy', 'balanced_accuracy'])
 
-print(scores["test_accuracy"].mean())
-print(scores["test_balanced_accuracy"].mean())
-
+print('Mean Accuracy: %f' % scores['test_accuracy'].mean())
+print('Mean Balanced Accuracy: %f' % scores['test_balanced_accuracy'].mean())
 
 ###############################################################################
 # Other kind of metrics allows us to evaluate how good our model is to detect
@@ -84,15 +124,12 @@ print(scores["test_balanced_accuracy"].mean())
 #
 # For this metric to work, we need to define which are our `positive` values.
 # In this example, we are interested in detecting `versicolor`.
-precision_scores = run_cross_validation(
-    X=X,
-    y=y,
-    data=df_unbalanced,
-    model="svm",
-    preprocess="zscore",
-    problem_type="classification",
-    seed=42,
-    scoring="precision",
-    pos_labels="versicolor",
-)
-print(precision_scores["test_score"].mean())
+#
+# Note that if only one scoring is required, the returned name is 'test_score'
+# independently of which scoring was requested.
+
+scores = run_cross_validation(
+    X=X, y=y, data=df_unbalanced, model=model, preprocess=preprocess, cv=cv,
+    seed=42, scoring='precision', pos_labels='versicolor')
+
+print('Mean Precision %f ' % scores['test_score'].mean())
