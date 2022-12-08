9b9a2d9fbbe48dda8413304355a9f31c4c8ab764
Vera Komeyer <v.komeyer@fz-juelich.de>
2022-12-08 14:23:57 +0100

    Add test for new metric

--- julearn/scoring/tests/test_metrics.py
+++ julearn/scoring/tests/test_metrics.py
@@ -1,7 +1,14 @@
-from julearn.scoring.metrics import r2_corr
+from julearn.scoring.metrics import r2_corr, r_pearson
 
 
 def test_r2_corr():
     """Test r2_corr."""
     assert r2_corr([1, 2, 3, 4], [1, 2, 3, 4]) == 1
     assert r2_corr([1, 2, 3, 4], [2, 3, 4, 5]) == 1
+
+
+def test_r_pearson():
+    """Test r_pearson."""
+    assert r_pearson([0, 1, 2, 4, 5, 6], [0, 1, 2, 4, 5, 6]) == 1
+    assert r_pearson([0, 1, 2, 4, 5, 6], [6, 5, 4, 2, 1, 0]) == -1
+    assert r_pearson([0, 1, 2, 4, 5, 6], [1, 2, 3, 5, 6, 7]) == 1
