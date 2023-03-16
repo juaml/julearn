641ba936e21f2d7aafe60e85118e6e7ee1dee596
Vera Komeyer <v.komeyer@fz-juelich.de>
2023-03-16 14:25:56 +0100

    Add copybutton extension

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,7 @@ extensions = [
     "sphinx_multiversion",
     "numpydoc",
     "gh_substitutions",
+    "sphinx_copybutton",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
